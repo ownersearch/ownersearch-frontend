0a6109674b09ad345c901932448502dfa7e1c9b5
DaveRevay2 <daverevay@gmail.com>
2017-08-21 21:15:28 +1000

    add address

--- src/js/features/People/PeopleResults/PeopleResults.jsx
+++ src/js/features/People/PeopleResults/PeopleResults.jsx
@@ -13,19 +13,28 @@ export default class PeopleResults extends Component {
     const { people, className } = this.props
     return (
       <div className={ className }>
-        { people && people.data && people.data.map(person => (
-          <div className={ cn('layout-row', s.result) }>
-            <div className="flex-40">{ person.name && person.name.givenName } { person.name && person.name.surname }</div>
-            <div className="flex-30">
-              { person.physicalAddress && person.physicalAddress.suburb }
+        { people && people.data && people.data.map(person => {
+          const address = person.physicalAddress && [
+            person.physicalAddress.streetNumber,
+            person.physicalAddress.streetName,
+            person.physicalAddress.streetType,
+            person.physicalAddress.suburb,
+            person.physicalAddress.postcode,
+          ].join(' ')
+          return (
+            <div className={ cn('layout-row', s.result) }>
+              <div className="flex-20">{ person.name && person.name.givenName } { person.name && person.name.surname }</div>
+              <div className="flex-60">
+                { address }
+              </div>
+              <div className="flex-20" style={ { textAlign: 'right' } }>
+                { person.contacts && person.contacts.map(contact => (
+                  <span>{ contact.displayValue }</span>
+                ))}
+              </div>
             </div>
-            <div className="flex-30" style={ { textAlign: 'right' } }>
-              { person.contacts && person.contacts.map(contact => (
-                <span>{ contact.displayValue }</span>
-              ))}
-            </div>
-          </div>
-        )) }
+          )
+        }) }
       </div>
     )
   }
